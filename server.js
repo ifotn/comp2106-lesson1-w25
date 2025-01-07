4aaf9307f11789e5a100f60adb907be9b3fd2626
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-01-07 14:13:30 -0500

    l1/2 - read html file

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+// import necessary libs
 const http = require('http');
+const fs = require('fs');
 
 // start web server & listen for http requests
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
+
+    fs.readFile('index.html', 'utf8', (err, fileContents) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.write(fileContents);        
+        }
+        res.end();
+    });
+     
+    //res.end('<h1>Hello World</h1>');
 }).listen(3000);
 
-console.log('Server running on port 3000');
+console.log('Server running on port 3000'); 
